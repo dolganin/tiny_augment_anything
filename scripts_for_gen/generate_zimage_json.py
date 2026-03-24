2835c3cb3915132ecd2b0993f143db35697c0225
FedorTikunov <fedor.tikunov@gmail.com>
2026-03-25 01:03:31 +0700

    minor fix

--- scripts_for_gen/generate_zimage_json.py
+++ scripts_for_gen/generate_zimage_json.py
@@ -190,8 +190,11 @@ class ZImageGenerator:
             num_inference_steps=steps,
             guidance_scale=guidance_scale,
             generator=self._generator(seed),
-            cross_attention_kwargs={"scale": self.lora_scale},
-        ).images[0]
+        )
+        if self.lora_path:
+            kwargs["cross_attention_kwargs"] = {"scale": self.lora_scale}
+
+        return pipe(**kwargs).images[0]
 
     def generate_inpaint(
         self,
@@ -214,8 +217,11 @@ class ZImageGenerator:
             num_inference_steps=steps,
             guidance_scale=guidance_scale,
             generator=self._generator(seed),
-            cross_attention_kwargs={"scale": self.lora_scale},
-        ).images[0]
+        )
+        if self.lora_path:
+            kwargs["cross_attention_kwargs"] = {"scale": self.lora_scale}
+
+        return pipe(**kwargs).images[0]
 
 
 def main():
